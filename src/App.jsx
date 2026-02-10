ec4e64a0c846034426393d82a8c6ea1b3ef4fc8f
VSV24 <134763629+VSV24@users.noreply.github.com>
2026-02-11 00:07:20 +0530

    Added CheckoutPage and TrackingPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage'
 import './App.css'
 import CheckoutPage from './pages/CheckoutPage'
+import OrdersPage from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='checkout' element={<CheckoutPage/>} />
+        <Route path='orders' element={<OrdersPage/>} />
+        <Route path='Tracking' element={<TrackingPage/>} />
       </Routes>
     </>
   )
