d14aa801ae989adfe74b61ea0ea559f6d98688f1
VSV24 <134763629+VSV24@users.noreply.github.com>
2026-02-14 14:19:13 +0530

    Implement product component with add to cart functionality; refactor HomePage and ProductsGrid to utilize new component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,20 +10,27 @@ import TrackingPage from './pages/tracking/TrackingPage'
 function App() {
   const [cart, setCart] = useState([])
   const [orders, setOrders] = useState([])
+
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product')
+    setCart(response.data)
+  }
+
+  const loadOrders = async () => {
+    const response = await axios.get('/api/orders?expand=products')
+    setOrders(response.data)
+  }
   
   useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-    .then((res) => setCart(res.data))
-    .catch(err => console.error('Failed to load cart-items', err))
+    loadCart()
 
-    axios.get('/api/orders?expand=products')
-    .then((res) => setOrders(res.data))
-  },[])
+    loadOrders()
+  })
 
   return(
     <>
       <Routes>
-        <Route path='/' element={<HomePage cart={cart} />} />
+        <Route path='/' element={<HomePage cart={cart} loadCart={loadCart} />} />
         <Route path='checkout' element={<CheckoutPage cart={cart} />} />
         <Route path='orders' element={<OrdersPage cart={cart} orders={orders} />} />
         <Route path='/tracking/:orderId/:productId' element={<TrackingPage cart={cart} orders={orders}/>} />
